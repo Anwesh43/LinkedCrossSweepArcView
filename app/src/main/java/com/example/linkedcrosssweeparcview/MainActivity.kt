13792b4f173a4862d26f071fc02aa8f3a815e031
Anwesh43 <anweshthecool0@gmail.com>
2020-11-28 21:16:48 +0530

    adding fullscreen and activity

--- app/src/main/java/com/example/linkedcrosssweeparcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcrosssweeparcview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedcrosssweeparcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.crosssweeparcview.CrossSweepArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CrossSweepArcView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
