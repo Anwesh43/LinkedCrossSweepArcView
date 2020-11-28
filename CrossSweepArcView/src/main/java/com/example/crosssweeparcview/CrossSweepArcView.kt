126b51bc3591a04200b2adc03d0294296e351814
Anwesh43 <anweshthecool0@gmail.com>
2020-11-28 20:48:21 +0530

    adding renderer in view

--- CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
+++ CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
@@ -69,14 +69,16 @@ fun Canvas.drawCSANode(i : Int, scale : Float, paint : Paint) {
 
 class CrossSweepArcView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -198,8 +200,9 @@ class CrossSweepArcView(ctx : Context) : View(ctx) {
 
         private val csa : CrossSweepArc = CrossSweepArc(0)
         private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             csa.draw(canvas, paint)
             animator.animate {
