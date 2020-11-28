8677d0bbf95c93317043ba13ba85aeb2454f7697
Anwesh43 <anweshthecool0@gmail.com>
2020-11-28 20:14:42 +0530

    adding Canvas extension methods

--- CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
+++ CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
@@ -31,3 +31,38 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawCrossSweepArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf.divideScale(2, parts))
+    for (j in 0..1) {
+        val lSize : Float = size * sf.divideScale(0, parts)
+        save()
+        rotate(90f * j)
+        drawLine(0f, -lSize, 0f, lSize, paint)
+        restore()
+        save()
+        drawArc(
+            RectF(-size, -size, size, size),
+            180f * j - deg,
+            deg * sf.divideScale(1, parts),
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCSANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCrossSweepArc(scale, w, h, paint)
+}
