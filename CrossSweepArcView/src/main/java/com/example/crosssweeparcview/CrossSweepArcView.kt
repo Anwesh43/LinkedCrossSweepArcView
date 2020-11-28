7000a47efb5c70031c836c5f92ba0ef884949812
Anwesh43 <anweshthecool0@gmail.com>
2020-11-28 20:35:38 +0530

    adding CSANode class

--- CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
+++ CrossSweepArcView/src/main/java/com/example/crosssweeparcview/CrossSweepArcView.kt
@@ -129,4 +129,45 @@ class CrossSweepArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSANode(var i : Int, val state : State = State()) {
+
+        private var next : CSANode? = null
+        private var prev : CSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size- 1) {
+                next = CSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCSANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSANode {
+            var curr : CSANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
